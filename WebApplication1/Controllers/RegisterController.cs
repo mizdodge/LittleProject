7e66f367a0d9cb29cb29d711ed11339d4ffcb9df
Mizan Hidayat <missdodge22@gmail.com>
2023-12-22 11:55:15 +0700

    Push create for register

---
+++ WebApplication1/Controllers/RegisterController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ILogger<RegisterController> _logger;
+        public IConfiguration Configuration;
+        public RegisterController(ILogger<RegisterController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+        // GET: RegisterController
+        public ActionResult Index()
+        {
+            ViewBag.pathurl = Configuration["UserConfig:PathAPI"];
+            return View();
+        }
+
+        public async Task<IActionResult> CheckUsername(string username)
+        {
+
+            var login = new LoginModel()
+            {
+                username = username,
+                password=""
+            };
+            var json = JsonConvert.SerializeObject(login);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
+            {
+                return true;
+            };
+            HttpClient client = new HttpClient(clientHandler);
+            HttpResponseMessage result = await client.GetAsync(Configuration["UserConfig:PathAPI"] + "/auth/Login/"+username);
+            var jsonresult = await result.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject(jsonresult);
+            return Ok(response);
+        }
+
+        // GET: RegisterController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RegisterController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegisterController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegisterController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RegisterController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegisterController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RegisterController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
